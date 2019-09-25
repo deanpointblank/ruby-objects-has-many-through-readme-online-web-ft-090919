db2e8a6d6e6b19951c492d69c183621120a7d7f2
Dean L Betty <deanlloyd64@gmail.com>
2019-09-25 23:11:41 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,16 @@
 class Customer
+  attr_reader :name, :age
+  
+  @@all = []
+  
+  def initialize(name, age)
+    @age = age
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    
+  end
   
 end
